ad155de7ca50bcb253591cd522bcb9d85e9372ff
jasonwu1994 <jasonwu1994@yahoo.com.tw>
2021-03-02 02:08:55 +0800

    DAO update

---
+++ src/main/java/com/authority_content/model/Authority_ContentBean.java
@@ -0,0 +1,31 @@
+package com.authority_content.model;
+
+public class Authority_ContentBean {
+
+    private Integer authority_content_id;
+    private String authority_content;
+
+    @Override
+    public String toString() {
+        return "authority_contentBean{" +
+                "authority_content_id=" + authority_content_id +
+                ", authority_content='" + authority_content + '\'' +
+                '}';
+    }
+
+    public Integer getAuthority_content_id() {
+        return authority_content_id;
+    }
+
+    public void setAuthority_content_id(Integer authority_content_id) {
+        this.authority_content_id = authority_content_id;
+    }
+
+    public String getAuthority_content() {
+        return authority_content;
+    }
+
+    public void setAuthority_content(String authority_content) {
+        this.authority_content = authority_content;
+    }
+}
