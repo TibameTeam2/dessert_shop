67d4f458bb5b5957c46e9dcbbc610548b740662d
Cheryl_Chao <radiancy@hotmail.com>
2021-03-04 13:00:20 +0800

    ProductImageDAO修改

--- src/main/java/com/product_image/model/ProductImageBean.java
+++ src/main/java/com/product_image/model/ProductImageBean.java
@@ -2,16 +2,17 @@ package com.product_image.model;
 
 import java.io.InputStream;
 import java.sql.Blob;
+import java.util.Arrays;
 
 public class ProductImageBean {
 	private Integer image_id;
 	private Integer product_id;
-	private Blob product_image;
+	private byte[] product_image;
 	
 	@Override
 	public String toString() {
-		return "product_imageBean [image_id=" + image_id + ", product_id=" + product_id + ", product_image="
-				+ product_image + "]";
+		return "ProductImageBean [image_id=" + image_id + ", product_id=" + product_id + ", product_image="
+				+ Arrays.toString(product_image) + "]";
 	}
 
 	public Integer getImage_id() {
@@ -29,14 +30,15 @@ public class ProductImageBean {
 	public void setProduct_id(Integer product_id) {
 		this.product_id = product_id;
 	}
-	
-	public Blob getProduct_image() {
+
+	public byte[] getProduct_image() {
 		return product_image;
 	}
 
-	public void setProduct_image(Blob product_image) {
+	public void setProduct_image(byte[] product_image) {
 		this.product_image = product_image;
 	}
+	
 	
 	
 }
