677d0c738069e449afd3ffbdf494f765e4ad0571
jasonwu1994 <jasonwu1994@yahoo.com.tw>
2021-03-13 20:48:20 +0800

    feat: JavaMail

---
+++ src/test/java/ReflectTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class ReflectTest {
+    @Test
+    public void arrayTest() throws Exception {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+
+        array.add(10);
+        array.add(20);
+//        array.add("hello");
+        System.out.println(array);
+
+        Class c = array.getClass();
+
+
+
+        Method m = c.getMethod("add", Object.class);
+        m.invoke(array,"hello");
+        m.invoke(array,9.222);
+        System.out.println(array);
+
+    }
+}
