19c272cfb66c79997f7165606d1efb7cd6185164
SLNBlade <sam199483819sam@gmail.com>
2021-03-24 16:16:44 +0800

    no message

--- src/test/java/MailTest.java
+++
@@ -1,9 +0,0 @@
-import cn.hutool.extra.mail.MailUtil;
-import org.junit.Test;
-
-public class MailTest {
-    @Test
-    public void sendMail() {
-        MailUtil.send("jasonwu1994@gmail.com", "嗜甜，信箱驗證", "❤❤❤", false);
-    }
-}
