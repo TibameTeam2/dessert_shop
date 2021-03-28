76c58b91597e2d5dc1d70470c8a349dc20d0758c
kennykenny789 <jtkennyg@gmail.com>
2021-03-28 17:11:50 +0800

    OrderMaster做了一點點

---
+++ src/main/java/com/order_master/model/OrderMasterService.java
@@ -0,0 +1,60 @@
+package com.order_master.model;
+
+import java.util.List;
+
+public class OrderMasterService {
+
+	private OrderMasterDao dao;
+
+	public OrderMasterService() {
+		dao = new OrderMasterDaoImpl();
+	}
+
+	public OrderMasterBean addOrderMaster(String member_account, Integer payment_method, Integer coupon_id,
+			Integer order_status, String invoice_number, Integer order_total, String order_remarks) {
+
+		OrderMasterBean orderMasterBean = new OrderMasterBean();
+		orderMasterBean.setMember_account(member_account);
+		orderMasterBean.setPayment_method(payment_method);
+		orderMasterBean.setCoupon_id(coupon_id);
+		orderMasterBean.setOrder_status(order_status);
+		orderMasterBean.setInvoice_number(invoice_number);
+		orderMasterBean.setOrder_total(order_total);
+		orderMasterBean.setOrder_remarks(order_remarks);
+		dao.insert(orderMasterBean);
+
+		return orderMasterBean;
+
+	}
+	
+	
+	public OrderMasterBean updateOrderMaster(Integer payment_method, Integer coupon_id,
+			Integer order_status, String invoice_number, Integer order_total, String order_remarks) {
+				
+		OrderMasterBean orderMasterBean = new OrderMasterBean();
+		orderMasterBean.setPayment_method(payment_method);
+		orderMasterBean.setCoupon_id(coupon_id);
+		orderMasterBean.setOrder_status(order_status);
+		orderMasterBean.setInvoice_number(invoice_number);
+		orderMasterBean.setOrder_total(order_total);
+		orderMasterBean.setOrder_remarks(order_remarks);
+		dao.update(orderMasterBean);
+		
+		return orderMasterBean;
+		
+	}
+	
+	
+	public void deleteOrderMaster(Integer order_master_id) {
+		dao.delete(order_master_id);
+	}
+	
+	
+	public OrderMasterBean getOneOrderMaster(Integer order_master_id) {
+		return dao.findByPrimaryKey(order_master_id);
+	}
+	
+	public List<OrderMasterBean> getAll(){
+		return dao.getAll();
+	}
+}
