79f94fefd4f38f129b7f0f7f9bbdc283e3aee85b
SLNBlade <sam199483819sam@gmail.com>
2021-04-06 05:30:39 +0800

    cart後端：進網頁取資料+商品圖src  用src取商品圖 增減商品數量 刪除商品OK  剩優惠碼生成優惠券部分(等浩淪改好資料庫)

--- src/main/java/com/cart/model/CartProductDAO.java
+++ src/main/java/com/cart/model/CartProductDAO.java
@@ -1,5 +1,6 @@
 package com.cart.model;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -21,14 +22,14 @@ public class CartProductDAO {
 	private String passwd = JDBCUtil.password;
 	
 	//拿cart_id, cart.product_id, product_quantity, product_name, product_price
-	public List selectByMemberAccount(String member_account) {
+	public List<CartProductBean> selectByMemberAccount(String member_account) {
 		String SELECT_ALL =
-			"SELECT cart_id, cart.product_id, product_quantity, product_name, product_price FROM sweet.cart left join product on product.product_id = cart.product_id where member_account = ?;";
+			"SELECT cart_id, cart.product_id, product_quantity, product_name, product_price FROM sweet.cart left join product on product.product_id = cart.product_id where member_account = ?";
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		
-		List list = new ArrayList();
+		List<CartProductBean> list = new ArrayList<CartProductBean>();
 		CartProductBean cpBean = null;
 		
 		
@@ -83,25 +84,25 @@ public class CartProductDAO {
 	}
 	
 	
-	//拿coupon_id, coupon_text_content
-	public List selectCouponByMemberAccount(String member_account) {
+	//拿coupon
+	public List<CouponBean> selectCouponByMemberAccount(String member_account) {
 
 		Connection con = null;
 		PreparedStatement pstmt = null;
 
-		String SELECT = "SELECT * FROM coupon where member_account = ?";
+		String SELECT_ALL = "SELECT * FROM sweet.coupon where member_account = ?";
 
 		CouponBean CB;
 		ResultSet rs = null;
 
-		List list_couponBean = new ArrayList();
+		List<CouponBean> list_couponBean = new ArrayList<CouponBean>();
 
 		try {
 			Class.forName(driver);
 
 			con = DriverManager.getConnection(url, userid, passwd);
 
-			pstmt = con.prepareStatement(SELECT);
+			pstmt = con.prepareStatement(SELECT_ALL);
 			
 			pstmt.setString(1, member_account);
 			
@@ -150,7 +151,162 @@ public class CartProductDAO {
 	}
 	
 	
-	//拿img的src的String
+	//拿第一張圖片資料流
+	public InputStream getFirstImageByProductId(Integer product_id) {
+		String SELECT_FIRST =
+				"SELECT product_image FROM sweet.product_image where product_id = ? LIMIT 1";
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			
+			ResultSet rs = null;
+			InputStream product_image = null;
+			
+			try {
+				Class.forName(driver);
+				con = DriverManager.getConnection(url, userid, passwd);
+				pstmt = con.prepareStatement(SELECT_FIRST);
+				
+				pstmt.setInt(1, product_id);
+				
+				rs = pstmt.executeQuery();
+				
+
+				if (rs.next()) {
+					product_image = rs.getBinaryStream("product_image");
+				}
+				
+				
+				// Handle any driver errors
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException("Couldn't load database driver. "
+						+ e.getMessage());
+				// Handle any SQL errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. "
+						+ se.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+			
+			
+		return product_image;
+		
+	}
+	
+	
+	//update購物車商品數量
+	public void updateProductQuantity(Integer cart_id, Integer product_quantity) {
+		String UPDATE =
+				"UPDATE sweet.cart set product_quantity = ? where cart_id = ?";
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			
+
+			
+			try {
+				Class.forName(driver);
+				con = DriverManager.getConnection(url, userid, passwd);
+				pstmt = con.prepareStatement(UPDATE);
+				
+				pstmt.setInt(1, product_quantity);
+				pstmt.setInt(2, cart_id);
+				
+				pstmt.executeUpdate();
+				
+				System.out.println(1);
+				
+				// Handle any driver errors
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException("Couldn't load database driver. "
+						+ e.getMessage());
+				// Handle any SQL errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. "
+						+ se.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+				
+	}
+	
+	
+	//delete商品
+	public void delete(Integer cart_id) {
+		String DELETE =
+			"DELETE FROM sweet.cart where cart_id = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(DELETE);
+			
+			pstmt.setInt(1, cart_id);
+			
+			pstmt.executeUpdate();
+			
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		
+	}
+	
+	
+	
+	
 	
 	
 
