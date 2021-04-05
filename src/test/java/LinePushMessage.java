c07db8e5fd1ac737fe8c339173f1e7a2cb9b7e19
jasonwu1994 <jasonwu1994@yahoo.com.tw>
2021-04-06 02:33:12 +0800

    feat:1.Line即時推播方法已封裝 2.my-account.html 加入綁定Line帳號的按鈕 3.GCP上架LineBot伺服器，使用Python寫後端，資料庫使用SQLite 4.compare.html比較商品頁大致完成

---
+++ src/test/java/LinePushMessage.java
@@ -0,0 +1,43 @@
+import cn.hutool.core.codec.Base64;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.symmetric.AES;
+import cn.hutool.http.HttpUtil;
+import com.google.gson.Gson;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LinePushMessage {
+    final String AES_KEY = "mhWv/FOMFa6MPCCBjLcnnA==";
+    final String url="https://sc.linebotweb.xyz:5002/pushMessage";
+    @Test
+    public void pushMessageTest(){
+        String member_account="111163633743988055654";
+        String message="提醒您!明日下午3點有訂位哦!!";
+
+        System.out.println(pushMessage(member_account,message));
+    }
+    public String pushMessage(String member_account,String message){
+        Map<String, String> content = new HashMap<>();
+        content.put("member_account",member_account);
+        content.put("message",message);
+        content.put("time", new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new java.sql.Timestamp(System.currentTimeMillis())));
+        Gson gson = new Gson();
+        String contentJson = gson.toJson(content);
+//        System.out.println("contentJson = " + contentJson);
+        // 使用AES加密
+        AES aes = SecureUtil.aes(Base64.decode(AES_KEY));
+        String encrypt = aes.encryptBase64(contentJson);
+
+        System.out.println("encrypt = " + encrypt);
+
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("token", encrypt);
+
+        String result= HttpUtil.post(url, paramMap);
+//        System.out.println("result = " + result);
+        return result;
+    }
+}
