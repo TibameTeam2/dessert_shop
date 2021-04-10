e7676e209ba1340c10448c3612ceed73fdbe4f1a
YoaYun <winnie19931029@gamil.com>
2021-04-11 01:52:47 +0800

    Revert "commit"
    This reverts commit 81c1760d6aea868fd17469a7194c66b97133f25c.

--- src/main/java/com/product/controller/ProductServlet_front.java
+++ src/main/java/com/product/controller/ProductServlet_front.java
@@ -158,13 +158,6 @@ public class ProductServlet_front extends BaseServlet {
     	
     }
     
-
-    public void checkProductName(HttpServletRequest req, HttpServletResponse res) {
-    	//獲取數據
-    	String product_name = req.getParameter("product_name");
-//    	ProductBean productBean = 
-    	
-    }
     
     
     
